a4315fcb4df26aaf429e7d81173919f0cccb697d
Thinura-neth <thinurabrother333@gmail.com>
2025-11-18 16:32:11 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyBoZK_G8DWfNSKoKOnJZAWRr0gqDbL_mNU",
+  authDomain: "edutech-database.firebaseapp.com",
+  databaseURL: "https://edutech-database-default-rtdb.firebaseio.com",
+  projectId: "edutech-database",
+  storageBucket: "edutech-database.firebasestorage.app",
+  messagingSenderId: "5848137290",
+  appId: "1:5848137290:web:bbc4132a5b23ae25abf0b3",
+  measurementId: "G-T8XE48VQL7"
+};
+
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
